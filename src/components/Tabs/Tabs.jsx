f74fa1a9885eec2390623ed973c48fd949796dca
gorbachovam00 <gorbachovamaria00@gmail.com>
2024-12-27 19:37:26 +0200

    add solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,24 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTab, handleTabClick }) => (
+  <div className="tabs is-boxed">
+    <ul>
+      {tabs.map(tab => (
+        <li
+          className={activeTab === tab.id ? 'is-active' : ''}
+          data-cy="Tab"
+          key={tab.id}
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={event => {
+              event.preventDefault();
+              handleTabClick(tab.id);
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
